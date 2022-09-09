703f5e3c8fb12307fa4d416ee416674398c02aa5
Your Name <laiseyjohns@gmail.com>
2022-09-09 10:43:27 -0700

    the correct task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,7 @@
  *
  * Return:Always 0 (success)
  */
+
 int main(void)
 {
 	char a;
@@ -15,8 +16,9 @@ int main(void)
 	float e;
 
 	printf("Size of a char:%d byte(s)\n",sizeof(a));
-	printf("Size of an int:%d byte(s)\n",sizeof(b));
-	printf("Size of a long int:%d byte(s)\n",sizeof(c));
-        printf("Size of an long long int:%d byte(s)\n",sizeof(d));  
-        printf("Size of a float:%d byte(s)\n",sizeof(e));                          
-        return (0);
+	printf("size of an int:%d byte(s)\n",sizeof(b));
+	printf("size of a long int:%d byte(s)\n",sizeof(c));
+	printf("size of a long long int:%d byte(s)\n",sizeof(d));
+	printf("size of a float:%d byte(s)\n",sizeof(e));
+	return 0;
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-task 5 using printf
+task 6
