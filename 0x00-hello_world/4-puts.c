a010d00466ba92ff38185adc7bae30925590a371
Your Name <laiseyjohns@gmail.com>
2022-09-08 18:38:30 -0700

    correcction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main-printing output
-*use puts not printf
-*return always 0 is successful
-*/
+ *main-printing output
+ *
+ *return always 0 is successful
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
- return (0);
+   puts("\"programming is like building a multilingual puzzle");
+    return (0);
 }
