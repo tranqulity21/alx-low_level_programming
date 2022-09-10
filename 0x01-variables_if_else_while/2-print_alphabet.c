1effd3221e929ce532fa310a485236fd455bd994
Your Name <laiseyjohns@gmail.com>
2022-09-10 06:35:52 -0700

    task 2 insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,13 @@
  * Return; 0
  */
 int main(void)
-{	
+ {	
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
-}
+	{
+		putchar(ch);
+ 	}	
+        putchar('\n');
+        return (0);
+ }
