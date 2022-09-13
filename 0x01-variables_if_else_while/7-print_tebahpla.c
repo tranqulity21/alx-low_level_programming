7057007cfa86ecb63c57074af21e7563e6d88719
Your Name <laiseyjohns@gmail.com>
2022-09-12 19:16:08 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - print if the number is positive,zero or negative
+ *
+ * description:using the main function
+ * this program prints positive,negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for(letter = 'z' ; letter >= 'a' ; letter --)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-prints all single digit numbers of base 10 starting from 0, followed by a new line.only allowed to use variable type char 
+Write a program that prints the lowercase alphabet in reverse, followed by a new line.
