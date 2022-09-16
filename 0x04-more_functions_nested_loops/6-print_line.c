316d2bc70e51dc5f74ecaf2fc0273cbf03fa34d0
Your Name <laiseyjohns@gmail.com>
2022-09-15 19:30:52 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,9 @@
  */
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
+	while (n-- > 0)
 	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-		 _putchar('\n');
+		_putchar('_');
 	}
-	else
-		_putchar('\n');
+	putchar('\n');
 }
