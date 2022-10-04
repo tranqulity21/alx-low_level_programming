e8246621214190174dccbb045a775882601bd946
Your Name <laiseyjohns@gmail.com>
2022-10-03 19:48:43 -0700

    ...

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,8 @@
  * @s1:First string
  * @s2:Second string
  *
- * Return: NULL in case of failure, but pointer to new string in case of success
+ * Return: NULL in case of failure,
+ * but pointer to new string in case of success
  */
 char *str_concat(char *s1, char *s2)
 {
